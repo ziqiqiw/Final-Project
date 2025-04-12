6718c6797db86b0a3ee1e586ef7e6ba40678bae1
Ziqiqi Wang <ziqiqiw@umich.edu>
2025-04-12 11:38:19 +0800

    Update dataloader.py

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,15 +1,18 @@
 import pandas as pd
 
 
-def load_remote_huggingface_dataset(dataset_name: str = "David-Egea/Creditcard-fraud-detection") -> dict:
-    
+def load_remote_huggingface_dataset(dataset_name: str = "David-Egea/Creditcard-fraud-detection") -> tuple:
     from datasets import load_dataset
     ds = load_dataset(dataset_name)
-    return ds
+    
+    df = pd.DataFrame(ds['train'])  # Convert the dataset to a pandas DataFrame
+    X = df.drop("Class", axis=1)
+    y = df["Class"]
+    return X, y
+
 
 def load_local_dataset(file_path: str = "../data/creditcard.csv") -> tuple:
-    
     data = pd.read_csv(file_path)
-    X = data.drop("Class", axis=1) 
+    X = data.drop("Class", axis=1)
     y = data["Class"]
     return X, y
